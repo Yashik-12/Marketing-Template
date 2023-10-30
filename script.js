8aa3dbb45f3fdcfa5d0480ea2aa143cf7bc89b42
yashikm <yashikm@axioned.com>
2023-10-30 18:10:53 +0530

    Made changes in js

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 const hamburger = document.querySelector(".hamburger"),
     navContainer = document.querySelector(".navlists"),
     navLink = document.querySelectorAll(".navlists a"),
+    scrollTop = document.querySelector('.top'),
     html = document.querySelector("html");
 
-window.addEventListener('load',() => {
+window.addEventListener('load', () => {
+
+    // Scroll to top 
+    scrollTop.addEventListener('click', () => {
+        window.scrollTo({ top: 0 })
+    })
+
     // Hamburger Functionality
     hamburger.addEventListener("click", () => {
         hamburger.classList.toggle("active");
         navContainer.classList.toggle("active-nav");
         html.classList.toggle("html-scroll");
     })
-    
+
     //remove active
     navLink.forEach(e => {
         e.addEventListener("click", () => {
